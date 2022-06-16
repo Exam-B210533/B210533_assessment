47f124abfdf44f440bec27aa11c0822aa46e6622
Exam-B210533 <s2272790@ed.ac.uk>
2022-06-16 14:35:16 +0000

    Update Using R in Noteable update 3

--- RScripts/Using_R_in_Noteable.R
+++ RScripts/Using_R_in_Noteable.R
@@ -7,7 +7,7 @@
 
 #Here is an example of using R as a calculator to perform a numerical operation. 
 
-(30000000 + 70000000)^2 - 1*10/4
+(30000000 + 70000000)^2 - 1*10/400000000
 
 
 # Using the c() and cbind() base R functions to combine data values and objects respectively. 
